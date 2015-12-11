a0def59d54217e79083255ddea3954d96d6aade8
Ki-Yeul, Lee <ky.lee@navercorp.com>
2015-12-11 19:23:35 +0900

    add files for travis-ci

---
+++ testhubblemon.py
@@ -0,0 +1,16 @@
+
+
+from django.test import TestCase
+
+
+class SampleTest(TestCase):
+	def setUp(self):
+		print('setup')
+
+	def test_foo(self):
+		print('foo')
+		self.assertEqual(1, 1)
+
+	def test_bar(self):
+		print('bar')
+		#self.assertEqual(1, 2)
