e8c2dee4d5d609a43dadd6e4e7756953f8ccfeb5
YoungboShim <s1luet25@gmail.com>
2016-06-10 08:04:00 +0900

    before ver.ryu

--- graph/node.py
+++ graph/node.py
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-
+import random
 
 class graph_pool:
 	def __init__(self, position):
@@ -42,7 +42,7 @@ class graph_pool:
 
 
 class graph_node:
-	def __init__(self, id, name=''):
+	def __init__(self, id, name='', datas=[]):
 		self.id = id 
 		if name == '':
 			self.name = id
@@ -51,6 +51,16 @@ class graph_node:
 
 		self.links = []
 		self.color = 'EEEEEE'
+		result_datas = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+		datas = [5,8,7,1,4,0,0,0,0,0,0,0,0,0,0,0]
+		total = 0
+		i=0
+		for val in datas:
+			total += val
+			result_datas[i] = val
+			i += 1;
+		result_datas = list(map(lambda x: x/total*100, result_datas))
+		self.datas = result_datas
 
 	def link(self, node, edge_name='', color='000000'):
 		self.links.append( (node.name, edge_name, color) )
@@ -64,9 +74,15 @@ class cytoscape_renderer:
 
 		nodes = ''
 		edges = ''
-		datas = [20, 30, 50]
+		datas = []
+		for i in range(16):
+			datas.append(random.randrange(0,10))
+
 		for node in lists:
-			nodes += "{ data: { id:'%s', name:'%s', color: '#%s', datas: '%s' } },\n" % (node.id, node.name, node.color, datas)
+			nodes += "{ data: { id:'%s', name:'%s', color: '#%s'" % (node.id, node.name, node.color)
+			for i in range(16):
+				nodes += ", data"+str(i)+": '%s'" % (node.datas[i])
+			nodes += " } },\n"
 
 			link_no = 0
 			for link in node.links:
@@ -124,12 +140,40 @@ class cytoscape_renderer:
 					
 					
 					'pie-size': '90%%',
-        				'pie-1-background-color': '#696969',
-        				'pie-1-background-size': function(ele) { return ele.data('datas')[1]+ele.data('datas')[2] },
-        				'pie-2-background-color': '#A9A9A9',
-        				'pie-2-background-size': function(ele) { return ele.data('datas')[2] },
-       					'pie-3-background-color': '#D3D3D3',
-       					'pie-3-background-size': function(ele) { return ele.data('datas')[2] }
+					
+					'pie-1-background-color': '#A9A9A9',
+					'pie-1-background-size': 'data(data0)',
+					'pie-2-background-color': '#696969',
+					'pie-2-background-size': 'data(data1)',
+					'pie-3-background-color': '#A9A9A9',
+					'pie-3-background-size': 'data(data2)',
+					'pie-4-background-color': '#D3D3D3',
+					'pie-4-background-size': 'data(data3)',
+					'pie-5-background-color': '#33FF00',
+					'pie-5-background-size': 'data(data4)',
+					'pie-6-background-color': '#D3D3D1',
+					'pie-6-background-size': 'data(data5)',
+					'pie-7-background-color': '#99FFFF',
+					'pie-7-background-size': 'data(data6)',
+					'pie-8-background-color': '#FF0033',
+					'pie-8-background-size': 'data(data7)',
+					'pie-9-background-color': '#D3D3D3',
+					'pie-9-background-size': 'data(data8)',
+					'pie-10-background-color': '#FF3366',
+					'pie-10-background-size': 'data(data9)',
+					'pie-11-background-color': '#9933FF',
+					'pie-11-background-size': 'data(data10)',
+					'pie-12-background-color': '#33FF33',
+					'pie-12-background-size': 'data(data11)',
+					'pie-13-background-color': '#990033',
+					'pie-13-background-size': 'data(data12)',
+					'pie-14-background-color': '#D3D3D3',
+					'pie-14-background-size': 'data(data13)',
+					'pie-15-background-color': '#33FF00',
+					'pie-15-background-size': 'data(data14)',
+					'pie-16-background-color': '#FFFF00',
+					'pie-16-background-size': 'data(data15)'
+
 					}
 				    },
 				    {
