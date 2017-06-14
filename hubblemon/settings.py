e3d6571b89fac362ef5f22feba1e4016d472c0d7
punkyoon <cryptography@konkuk.ac.kr>
2017-06-14 18:40:22 +0000

    typo

--- hubblemon/settings.py
+++ hubblemon/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
 )
 
-MIDDLEWARE = [
+MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
